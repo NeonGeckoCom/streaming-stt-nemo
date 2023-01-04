8ce4eeb26368f3dfacb663d64196d659874542c6
NeonBohdan <bohdan@neon.ai>
2023-01-04 13:39:42 +0200

    Added version file

--- setup.py
+++ setup.py
@@ -1,9 +1,19 @@
 from setuptools import setup, find_packages
 
 
+
+with open("./version.py", "r", encoding="utf-8") as v:
+    for line in v.readlines():
+        if line.startswith("__version__"):
+            if '"' in line:
+                version = line.split('"')[1]
+            else:
+                version = line.split("'")[1]
+
+
 setup(
     name='streaming-stt-nemo',
-    version="0.0.1",
+    version=version,
     packages=find_packages(),
     python_requires='>3.7.0',
     install_requires=[
