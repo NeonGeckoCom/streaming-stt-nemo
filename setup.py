0e81c9d1cf2994740ad8fcc0e5059a9213d9ca10
NeonBohdan <bohdan@neon.ai>
2023-01-04 13:52:39 +0200

    Added requirements.txt

--- setup.py
+++ setup.py
@@ -1,5 +1,24 @@
 from setuptools import setup, find_packages
+from os import path, getenv
 
+
+
+def get_requirements(requirements_filename: str):
+    requirements_file = path.join(path.abspath(path.dirname(__file__)), "requirements", requirements_filename)
+    with open(requirements_file, 'r', encoding='utf-8') as r:
+        requirements = r.readlines()
+    requirements = [r.strip() for r in requirements if r.strip() and not r.strip().startswith("#")]
+
+    for i in range(0, len(requirements)):
+        r = requirements[i]
+        if "@" in r:
+            parts = [p.lower() if p.strip().startswith("git+http") else p for p in r.split('@')]
+            r = "@".join(parts)
+            if getenv("GITHUB_TOKEN"):
+                if "github.com" in r:
+                    r = r.replace("github.com", f"{getenv('GITHUB_TOKEN')}@github.com")
+            requirements[i] = r
+    return requirements
 
 
 with open("./version.py", "r", encoding="utf-8") as v:
@@ -16,7 +35,5 @@ setup(
     version=version,
     packages=find_packages(),
     python_requires='>3.7.0',
-    install_requires=[
-        "nemo_toolkit[asr]",
-    ],
+    install_requires=get_requirements("requirements.txt"),
 )
