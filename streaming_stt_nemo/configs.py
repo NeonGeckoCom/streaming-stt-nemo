657e4f253e2174c4ab497d398b7c251b389a9936
NeonBohdan <96498856+NeonBohdan@users.noreply.github.com>
2023-01-04 14:39:31 +0200

    Added scale in config (#8)

--- streaming_stt_nemo/configs.py
+++ streaming_stt_nemo/configs.py
@@ -26,6 +26,7 @@
 # NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 # SOFTWARE,  EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+
 languages = {
     "en": {
         "model": "stt_en_citrinet_512_gamma_0_25", 
